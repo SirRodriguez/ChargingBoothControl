fa21550d39f4bd767f3eefe89ef154a77407a7b4
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-09-11 23:49:02 -0700

    Added a login form to home page

--- CB_control/main/routes.py
+++ CB_control/main/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template, Blueprint, url_for, redirect
+from CB_control.main.forms import LoginForm
 
 main = Blueprint('main', __name__)
 
-@main.route("/")
-@main.route("/home")
+@main.route("/", methods=['GET', 'POST'])
+@main.route("/home", methods=['GET', 'POST'])
 def home():
-	return render_template("home.html")
+
+	form = LoginForm()
+	if form.validate_on_submit():
+		pass
+
+	return render_template("home.html", title="Home", form=form)
