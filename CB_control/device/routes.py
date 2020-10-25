6518514e283719791f5ac75f9d745f0c35c0e4c2
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-10-25 13:46:34 -0700

    Added admin key to routes for device info

--- CB_control/device/routes.py
+++ CB_control/device/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, redirect, url_for, render_template
-from flask_login import login_required
-from CB_control import  service_ip
+from flask_login import login_required, current_user, logout_user
+from CB_control import  service_ip, admin_key
 from CB_control.device.forms import LoginForm
 import requests
 
@@ -30,13 +30,24 @@ def confirm_removal(id, location):
 			return redirect(url_for('error.server_error'))
 
 		if payload.json()["user_verified"]:
+			# Admin key changed after check
+			admin_key.set_key(payload.json()["admin_key"])
+
 			# remove the device
 			try:
-				response = requests.delete(service_ip + '/site/remove_device/' + str(id))
+				response = requests.delete(service_ip + '/site/remove_device/' + str(id) + '/' + admin_key.get_key())
 			except:
 				flash("Unable to Connect to Server!", "danger")
 				return redirect(url_for('error.server_error'))
 
+			# Check admin Key is good
+			if response.status_code == 401:
+				if current_user.is_authenticated:
+					logout_user()
+				flash('Please login to access this page.', 'info')
+				return redirect(url_for('admin_user.admin_login'))
+
+			# Check the other response codes
 			if response.status_code == 204:
 				flash('Device has been successfuly removed!', 'success')
 			elif response.status_code == 400:
@@ -48,23 +59,23 @@ def confirm_removal(id, location):
 
 	return render_template("device/remove_confirm.html", title="Confirm Removal", form=form, location=location)
 
-# Depretiated
-# Remove a device from the service
-@device.route("/device/remove/<int:id>")
-@login_required
-def remove_device(id):
-	# remove the device
-	try:
-		response = requests.delete(service_ip + '/site/remove_device/' + str(id))
-	except:
-		flash("Unable to Connect to Server!", "danger")
-		return redirect(url_for('error.server_error'))
+# # Depretiated
+# # Remove a device from the service
+# @device.route("/device/remove/<int:id>")
+# @login_required
+# def remove_device(id):
+# 	# remove the device
+# 	try:
+# 		response = requests.delete(service_ip + '/site/remove_device/' + str(id))
+# 	except:
+# 		flash("Unable to Connect to Server!", "danger")
+# 		return redirect(url_for('error.server_error'))
 
-	if response.status_code == 204:
-		flash('Device has been successfuly removed!', 'success')
-	elif response.status_code == 400:
-		flash('Device was not found in the server!', 'danger')
-	else:
-		flash("Oops! Something happened and the device was not deleted.", "danger")
+# 	if response.status_code == 204:
+# 		flash('Device has been successfuly removed!', 'success')
+# 	elif response.status_code == 400:
+# 		flash('Device was not found in the server!', 'danger')
+# 	else:
+# 		flash("Oops! Something happened and the device was not deleted.", "danger")
 
-	return redirect(url_for('main.home'))
+# 	return redirect(url_for('main.home'))
--- CB_control/main/routes.py
+++ CB_control/main/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, url_for, redirect, flash
-from flask_login import login_required
-from CB_control import service_ip
+from flask_login import login_required, current_user, logout_user
+from CB_control import service_ip, admin_key
 import requests
 
 main = Blueprint('main', __name__)
@@ -15,11 +15,17 @@ def defualt():
 @login_required
 def home():
 	try:
-		payload = requests.get(service_ip + '/site/get_all')
+		payload = requests.get(service_ip + '/site/get_all/' + admin_key.get_key())
 	except:
 		flash("Unable to Connect to Server!", "danger")
 		return redirect(url_for('error.server_error'))
 
+	# Check admin Key is good
+	if payload.status_code == 401:
+		if current_user.is_authenticated:
+			logout_user()
+		flash('Please login to access this page.', 'info')
+		return redirect(url_for('admin_user.admin_login'))
 
 	device_id_list = payload.json()["device_id"]
 	location_list = payload.json()["location"]
